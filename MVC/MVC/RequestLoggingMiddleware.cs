718fb74e56aa3575a5ac731e51cb5dc445e2cf58
Peash <107058247+Peash02@users.noreply.github.com>
2026-01-16 12:41:39 +0530

    Add files via upload

---
+++ MVC/MVC/RequestLoggingMiddleware.cs
@@ -0,0 +1,24 @@
+﻿namespace MVC
+{
+    public class RequestLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RequestLoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            Console.WriteLine("------ Incoming Request ------");
+            Console.WriteLine($"Time   : {DateTime.Now}");
+            Console.WriteLine($"Method : {context.Request.Method}");
+            Console.WriteLine($"Path   : {context.Request.Path}");
+            Console.WriteLine("------------------------------");
+
+            //Pass request to next Middleware(MVC)
+            await _next(context);
+        }
+    }
+}
