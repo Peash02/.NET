9eba1133c7a978b9e8c437bfc3a49b1f891cd42a
Peash <107058247+Peash02@users.noreply.github.com>
2026-01-23 12:03:01 +0530

    Update StudentController.cs

--- Routing/Controllers/StudentController.cs
+++ Routing/Controllers/StudentController.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Routing.Controllers
 {
     public class StudentController : Controller
     {
-        public IActionResult Index()
+        [Route("Details/{id}")]
+        public IActionResult Details(int id)
         {
-            return View();
+            return Content("Student ID is: " + id);
+        }
+
+        [Route("Marks/{id}/{subject}")]
+        //Multiple Route parameters
+        public IActionResult Marks(int id , string subject)
+        {
+            return Content($"Student Id:{id}. Subject : {subject}. ");
+        }
+
+        [Route("Input/{id}")]
+        public IActionResult Input(int id , string name)
+        {
+            string message = $"Student Id:{id}, Student Name: {name}";
+            return Content(message);
         }
     }
 }
