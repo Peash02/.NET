87f5bc5bb9ea75f93de936540cafd3dfa9874be3
Peash <107058247+Peash02@users.noreply.github.com>
2026-01-23 12:49:15 +0530

    Add files via upload

---
+++ Razor/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Razor.Models;
+
+namespace Razor.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Product> products = new List<Product>
+            {
+                new Product { Id = 1, Name = "Laptop", Price = 55000, Quantity = 2,Description = "This is a High-End Laptop WIth RTX 6090 and Intel i12 Processor."},
+                new Product { Id = 2, Name = "SmartPhone", Price = 80000 , Quantity = 5 ,Description = "This is a High-End Smartphone with SnapDragon 50 Elite Ultra Chip with 10000mAH Battery."},
+                new Product { Id = 3, Name = "Headphones", Price = 20000 , Quantity = 10,Description = "This is a Headphones with ANC and 69mm Drivers in Red Colour and lasts 50 days on one charge."}
+            };
+
+            return View(products);
+        }
+    }
+}
