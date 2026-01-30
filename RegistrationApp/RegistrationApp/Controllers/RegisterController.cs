c6aa2ba9939089ee14d8734b744c989823178dd3
Peash <107058247+Peash02@users.noreply.github.com>
2026-01-30 12:47:56 +0530

    Add files via upload

---
+++ RegistrationApp/RegistrationApp/Controllers/RegisterController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RegistrationApp.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Models.Register user)
+        {
+            if (ModelState.IsValid)
+            {
+                return Content($"Registration Successful\n Name:{user.Name}\n Email:{user.Email}\n Password:{user.Password}\n Age:{user.Age}");
+
+            }
+
+            return View(user);
+        }
+    }
+}
