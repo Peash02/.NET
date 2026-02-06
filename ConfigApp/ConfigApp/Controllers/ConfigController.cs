803c1b981a3b0cee0fb02913add9a2f367cff700
Peash <107058247+Peash02@users.noreply.github.com>
2026-02-06 12:58:51 +0530

    Add files via upload

---
+++ ConfigApp/ConfigApp/Controllers/ConfigController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConfigApp.Controllers
+{
+    public class ConfigController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        public ConfigController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.AppName = 
+                _configuration["AppSettings:ApplicationName"];
+
+            ViewBag.Version =
+                _configuration["AppSettings:Version"];
+
+            ViewBag.Developer =
+                _configuration["AppSettings:Developer"];
+            return View();
+        }
+    }
+}
