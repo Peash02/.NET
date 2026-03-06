ac6cd3415a0442c5dba897bb272c40f0528fd168
Peash <107058247+Peash02@users.noreply.github.com>
2026-03-06 12:11:29 +0530

    Add files via upload

---
+++ RoleBase/RoleBase/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Rolebase.Controllers;
+
+namespace Rolebase.Controllers
+{
+    [Authorize(Roles = "User")]
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
